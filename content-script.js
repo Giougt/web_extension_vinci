1c381ad02a2a52c2365bf9756784336662268b60
Giougt <alexandre18golf@gmail.com>
2024-10-20 02:40:46 +0200

    first test and sucess without button

--- content-script.js
+++ content-script.js
@@ -1,1 +1,18 @@
-alert('Hello World!');
+//get value color text 
+
+//get value color background day schedule 
+
+const text_class_style = document.querySelectorAll(".b-dayview-day-container .b-calendar-cell .b-cal-event");
+text_class_style.forEach(function(element) {
+    element.style.color = "white";
+});
+
+const background_class_style = document.querySelectorAll(".b-dayview-day-container .b-calendar-cell .b-cal-event-body");
+background_class_style.forEach(function(element) {
+    element.style.backgroundColor = "black";
+});
+
+const background_extra_style = document.querySelectorAll(".b-cal-event");
+background_extra_style.forEach(function(element) {
+    element.style.backgroundColor = "black";
+});
