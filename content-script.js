08cad005c095fd8e05e785da821f555952347311
Giougt <alexandre18golf@gmail.com>
2024-10-24 23:55:22 +0200

    maj manifest and test

--- content-script.js
+++ content-script.js
@@ -1,6 +1,9 @@
 //get value color text 
 
 //get value color background day schedule 
+document.body.addEventListener("click",function(){
+    console.log("hello"); 
+})
 
 const text_class_style = document.querySelectorAll(".b-dayview-day-container .b-calendar-cell .b-cal-event");
 text_class_style.forEach(function(element) {
@@ -16,3 +19,5 @@ const background_extra_style = document.querySelectorAll(".b-cal-event");
 background_extra_style.forEach(function(element) {
     element.style.backgroundColor = "black";
 });
+
+
