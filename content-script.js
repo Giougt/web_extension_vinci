21dd86aef0c9b05463d8dc590dacb8db742653de
Giougt <alexandre18golf@gmail.com>
2024-10-28 23:59:12 +0100

    test on button

--- content-script.js
+++ content-script.js
@@ -1,6 +1,22 @@
 //get value color text 
 
 //get value color background day schedule 
+buttonE.document.addEventListener("click", function(){
+    const text_class_style = document.querySelectorAll(".b-dayview-day-container .b-calendar-cell .b-cal-event");
+    text_class_style.forEach(function(element) {
+        element.style.color = "white";
+});
+
+    const background_class_style = document.querySelectorAll(".b-dayview-day-container .b-calendar-cell .b-cal-event-body");
+    background_class_style.forEach(function(element) {
+        element.style.backgroundColor = "black";
+});
+
+    const background_extra_style = document.querySelectorAll(".b-cal-event");
+    background_extra_style.forEach(function(element) {
+        element.style.backgroundColor = "black";
+});
+})
 
 document.querySelector(".b-panel-content.b-sidebar-content.b-box-center.b-widget-scroller.b-resize-monitored.b-content-element.b-auto-container.b-flex-column").addEventListener("click",function(){
     const text_class_style = document.querySelectorAll(".b-dayview-day-container .b-calendar-cell .b-cal-event");
@@ -19,6 +35,8 @@ document.querySelector(".b-panel-content.b-sidebar-content.b-box-center.b-widget
 });
 
 });
+
+//button dark_mode 
 const buttonE = document.createElement("button");
 buttonE.innerText = ">/";
 
